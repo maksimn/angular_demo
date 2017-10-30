ace6c6fe056cd001e4750a7195179bb13a924d27
maksimn <maksim1989sovunion@yandex.ru>
2017-10-30 13:39:01 +0300

    vanilla project some pages

--- vanilla/app.js
+++ vanilla/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.use(express.static('./'));
+app.use(express.static('./pages/'));
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
