b5175d51dd358abd27dc326da0a4bf97a2c7d395
maksimn <maksim1989sovunion@yandex.ru>
2017-10-31 04:23:42 +0300

    index.html page

--- vanilla/app.js
+++ vanilla/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.use(express.static('./pages/'));
+app.use(express.static(__dirname + '/pages'));
+app.use(express.static(__dirname));
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
