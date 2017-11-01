6be31c17d7a12b332d5434e20aedcb5f4c7c6098
maksimn <maksim1989sovunion@yandex.ru>
2017-11-01 11:13:06 +0300

    to ejs template engine

--- vanilla/app.js
+++ vanilla/app.js
@@ -3,8 +3,17 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.use(express.static(__dirname + '/pages'));
+app.set('view engine', 'ejs');
+
 app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.render('../layout', { page: 'index' });
+});
+
+app.get('/:page', (req, res) => {
+    res.render('../layout', { page: req.params.page });
+});
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
