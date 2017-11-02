fcec37ff43c193d45948e6baa9a7ab3df169648f
maksimn <maksim1989sovunion@yandex.ru>
2017-11-02 18:12:02 +0300

    error page

--- vanilla/app.js
+++ vanilla/app.js
@@ -7,17 +7,23 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname));
 
+// app paths begin
 app.get('/', (req, res) => {
     res.render('layout', { page: 'index' });
 });
 
-const routes = ['/index', '/login', '/photos', '/profile', '/register'];
+const routes = ['/index', '/login', '/photos', '/profile', '/register', '/error'];
 
 routes.forEach(route => {
     app.get(route, (req, res) => {
         res.render('layout', { page: route.substring(1) });
     });
 });
+
+app.get('*', (req, res) => {
+    res.render('layout', { page: 'error' });
+});
+// app paths end
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
