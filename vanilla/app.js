e62d12225124a9b32dbca9f66b2ec7fe6d4a12b8
maksimn <maksim1989sovunion@yandex.ru>
2017-11-03 15:48:13 +0300

    adding security to this app: authMiddleware

--- vanilla/app.js
+++ vanilla/app.js
@@ -7,24 +7,42 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname));
 
+// List of app users
+// user schema: { id: some_id, name: some_name, token: some_token };
+const users = []; 
+
+const authMiddleware = (req, res, next) => {
+    const authToken = req.get('x-auth');
+    const isAuthorized = users.some(user => user.token === authToken);
+    const url = req.url;
+    
+    if (isAuthorized && (url === '/login' || url === '/register')) {
+        res.redirect('/photos');
+    } else if (!isAuthorized && (url === '/photos' || url === '/profile')) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
 // app paths begin
 app.get('/', (req, res) => {
     res.render('layout', { page: 'index' });
 });
 
-app.get('/login', (req, res) => {
+app.get('/login', authMiddleware, (req, res) => {
     res.render('layout', { page: 'login' });
 });
 
-app.get('/register', (req, res) => {
+app.get('/register', authMiddleware, (req, res) => {
     res.render('layout', { page: 'register' });
 });
 
-app.get('/photos', (req, res) => {
+app.get('/photos', authMiddleware, (req, res) => {
     res.render('layout', { page: 'photos' });
 });
 
-app.get('/profile', (req, res) => {
+app.get('/profile', authMiddleware, (req, res) => {
     res.render('layout', { page: 'profile' });
 });
 
