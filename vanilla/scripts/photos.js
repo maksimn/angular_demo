27e166a926b5f6b938df439cf12f1a6202e74061
maksimn <maksim1989sovunion@yandex.ru>
2017-11-09 13:42:25 +0300

    First photos page

--- vanilla/scripts/photos.js
+++ vanilla/scripts/photos.js
@@ -1,3 +1,46 @@
+var MAX_NUM_PHOTOS_PER_PAGE = 50;
+
+function createPhotoView(photoData) {
+    var photoView = document.createElement('div');
+    var photoImg = document.createElement('img');
+    var caption = document.createElement('div');
+
+    photoView.classList.add('photo-view');
+
+    photoImg.src = photoData.thumbnailUrl;
+    caption.innerText = photoData.title;
+
+    photoView.appendChild(photoImg);
+    photoView.appendChild(caption);
+    
+    return photoView;
+}
+
+function setupPagination(photosNumber) {
+    if (photosNumber > MAX_NUM_PHOTOS_PER_PAGE) {
+        var numberPaginationLinks = Math.ceil(photosNumber / MAX_NUM_PHOTOS_PER_PAGE);
+        var pagination = document.getElementById('pagination');
+        for (var i = 0; i < numberPaginationLinks; i++) {
+            var a = document.createElement('a');
+            a.href = '/photos#' + (i + 1);
+            a.innerText = (i + 1);
+            a.className = "pagination-link";
+            pagination.appendChild(a);
+        }
+    }
+}
+
+function showPhotos(photosData, begin, end) {
+    var photosView = document.getElementById('photos');
+
+    photosData
+        .slice(begin, end)
+        .forEach(function(photoData) {
+            var photoView = createPhotoView(photoData);
+            photosView.appendChild(photoView);
+        });
+}
+
 window.addEventListener('load', function() {
     var xhr = new XMLHttpRequest();
     
@@ -8,9 +51,11 @@ window.addEventListener('load', function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var photosData = JSON.parse(xhr.responseText);
 
-            console.log(photosData);
+            setupPagination(photosData.length);
+            showPhotos(photosData, 0, MAX_NUM_PHOTOS_PER_PAGE);
+            
         } else if (xhr.readyState === 4 && xhr.status === 400) {
-            console.log('error!!!!');
+            alert('Упс, не удалось получить фотографии :(');
         }
     };
 
