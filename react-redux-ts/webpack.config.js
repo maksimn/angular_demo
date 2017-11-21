0d7d6370badf1668900871876255eb7c1676a54a
maksimn <maksim1989sovunion@yandex.ru>
2017-11-21 04:52:48 +0300

    webpack config

--- react-redux-ts/webpack.config.js
+++ react-redux-ts/webpack.config.js
@@ -1,18 +1,19 @@
 var path = require('path');
 
 module.exports = {
-    entry: {
-    },
+    entry: path.resolve(__dirname, 'client/index.ts'),
     module: {
-      rules: [{
-          test: /\.ts$/,
-          use: 'ts-loader',
-          exclude: /node_modules/
-      }]
+        rules: [{
+            test: /\.ts$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
     },
     resolve: {
-      extensions: [ '.ts', '.js' ]
+        extensions: [ '.ts', '.js' ]
     },
     output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, 'client')
     }
 };
