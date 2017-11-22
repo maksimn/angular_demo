895838667ed44088442d168fa22c5a987c9b7d87
maksimn <maksim1989sovunion@yandex.ru>
2017-11-22 12:23:21 +0300

    IndexPage

--- react-redux-ts/client/index.tsx
+++ react-redux-ts/client/index.tsx
@@ -1,11 +1,30 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-interface HelloProps { compiler: string; framework: string; }
-
-const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+const IndexPage = () => <div>
+    <div className="container-fluid bg-primary">
+        <h2 className="text-white">TypeScript Демо</h2>
+    </div>
+    <div className="col-lg-4 col-lg-offset-4">
+        <h3>Страницы приложения</h3>
+        <ul className="nav navbar">
+            <li>
+                <a href="register">Форма регистрации пользователя.</a>
+            </li>
+            <li>
+                <a href="login">Форма логина пользователя.</a>
+            </li>
+            <li>
+                <a href="photos">Фотографии.</a>
+            </li>
+            <li>
+                <a href="profile">Профиль пользователя.</a>
+            </li>
+        </ul>
+    </div>
+</div>;
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <IndexPage />,
     document.getElementById("app")
 );
