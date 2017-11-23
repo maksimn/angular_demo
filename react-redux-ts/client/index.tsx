17973ae987a62eb33195478cea1b1563f55ef8cd
maksimn <maksim1989sovunion@yandex.ru>
2017-11-23 03:34:24 +0300

    IndexPage to a separate file

--- react-redux-ts/client/index.tsx
+++ react-redux-ts/client/index.tsx
@@ -1,30 +1,9 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-const IndexPage = () => <div>
-    <div className="container-fluid bg-primary">
-        <h2 className="text-white">TypeScript Демо</h2>
-    </div>
-    <div className="col-lg-4 col-lg-offset-4">
-        <h3>Страницы приложения</h3>
-        <ul className="nav navbar">
-            <li>
-                <a href="register">Форма регистрации пользователя.</a>
-            </li>
-            <li>
-                <a href="login">Форма логина пользователя.</a>
-            </li>
-            <li>
-                <a href="photos">Фотографии.</a>
-            </li>
-            <li>
-                <a href="profile">Профиль пользователя.</a>
-            </li>
-        </ul>
-    </div>
-</div>;
+import IndexPage from './components/IndexPage';
 
 ReactDOM.render(
     <IndexPage />,
-    document.getElementById("app")
+    document.getElementById('app')
 );
