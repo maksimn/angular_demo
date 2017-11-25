017d3eac74bd723bd7d2dfa56c5c9274f0b5d042
maksimm <mmaksmn@gmail.com>
2017-11-25 23:44:25 +0300

    RegisterForm stateless component

--- react-redux-ts/client/components/RegisterForm.tsx
+++ react-redux-ts/client/components/RegisterForm.tsx
@@ -1,26 +1,62 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import ValidationFieldError from '../interfaces/ValidationFieldError';
+import ValidationErrors from '../components/ValidationErrors';
 
-const RegisterForm = () => 
-    <div className="col-lg-3 col-lg-offset-4">
-        <h3>Регистрация</h3>
-        <form id="registerForm" method="post" action="register">
-            <div className="form-group">
-                <label htmlFor="username">Имя:</label>
-                <input type="text" className="form-control" name="username" id="username" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Пароль:</label>
-                <input type="password" className="form-control" name="password" id="password" />
-            </div>
-            <div className="form-group">
-                <label htmlFor="confirmPassword">Повторить пароль:</label>
-                <input type="password" className="form-control" name="confirmPassword" id="confirmPassword" />
-            </div>
-            <button type="submit" className="btn btn-primary">Отправить</button>
-            <Link className="btn btn-success" to="/login">Войти</Link>
-            <div id="validationErrors" className="alert alert-danger hidden" />
-        </form>
-    </div>;
+export interface RegisterFormProps {
+    onUsernameChange: (username: string) => void,
+    onPasswordChange: (password: string) => void,
+    onConfirmPasswordChange: (confirmPassword: string) => void,
+    onFormSubmit: () => void,
+    validationErrors: ValidationFieldError[]
+}
+
+const RegisterForm: React.StatelessComponent<RegisterFormProps> = (props) => {
+    const onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        props.onUsernameChange(event.target.value);
+    },
+    onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        props.onPasswordChange(event.target.value);
+    },
+    onConfirmPasswordChange =  (event: React.ChangeEvent<HTMLInputElement>) => {
+        props.onConfirmPasswordChange(event.target.value);
+    },    
+    onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        props.onFormSubmit();
+    },
+    {validationErrors} = props;
+
+    return (
+        <div className="col-lg-3 col-lg-offset-4">
+            <h3>Регистрация</h3>
+            <form id="registerForm" method="post" action="register"
+                onSubmit={onFormSubmit}>
+                <div className="form-group">
+                    <label htmlFor="username">Имя:</label>
+                    <input type="text" className="form-control" name="username" id="username"
+                        onChange={onUsernameChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Пароль:</label>
+                    <input type="password" className="form-control" name="password" id="password"
+                        onChange={onPasswordChange} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="confirmPassword">Повторить пароль:</label>
+                    <input type="password" className="form-control" name="confirmPassword" id="confirmPassword"
+                        onChange={onConfirmPasswordChange} />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                    Отправить
+                    </button>
+                <Link className="btn btn-success" to="/login">Войти</Link>
+
+                <ValidationErrors validationErrors={validationErrors} />
+
+            </form>
+        </div>
+    );
+};
 
 export default RegisterForm;
--- react-redux-ts/client/containers/RegisterForm.tsx
+++ react-redux-ts/client/containers/RegisterForm.tsx
@@ -3,10 +3,9 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 import {submitRegistrationData} from "../actions/authorization";
 import RegistrationData from '../interfaces/RegistrationData';
-import ValidationErrors from '../components/ValidationErrors';
+import RegisterFormComponent from '../components/RegisterForm';
 
 class RegisterForm extends React.Component<any, RegistrationData> {
     constructor(props: any){
@@ -21,62 +20,30 @@ class RegisterForm extends React.Component<any, RegistrationData> {
         });
     }
 
-    onUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({...this.state, username: event.target.value});
+    onUsernameChange = (username: string) => {
+        this.setState({...this.state, username});
     }
 
-    onPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({...this.state, password: event.target.value});
+    onPasswordChange = (password: string) => {
+        this.setState({...this.state, password});
     }
 
-    onConfirmPasswordChange =  (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({...this.state, confirmPassword: event.target.value});
+    onConfirmPasswordChange =  (confirmPassword: string) => {
+        this.setState({...this.state, confirmPassword});
     }
     
-    onFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
+    onFormSubmit = () => {
         this.props.dispatch(submitRegistrationData(this.state));
     }
 
     render() {
-        const { 
-            onUsernameChange, 
-            onPasswordChange, 
-            onConfirmPasswordChange, 
-            onFormSubmit
-        } = this;
-        const {validationErrors} = this.props;
+        const { onUsernameChange, onPasswordChange, onConfirmPasswordChange, onFormSubmit } = this;
+        const { validationErrors } = this.props;
+        
+        const props = { onUsernameChange, onPasswordChange, onConfirmPasswordChange, onFormSubmit, 
+            validationErrors };
 
-        return (
-            <div className="col-lg-3 col-lg-offset-4">
-                <h3>Регистрация</h3>
-                <form id="registerForm" method="post" action="register"
-                      onSubmit={ onFormSubmit }>
-                    <div className="form-group">
-                        <label htmlFor="username">Имя:</label>
-                        <input type="text" className="form-control" name="username" id="username"
-                               onChange={ onUsernameChange } />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="password">Пароль:</label>
-                        <input type="password" className="form-control" name="password" id="password"
-                               onChange={ onPasswordChange } />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="confirmPassword">Повторить пароль:</label>
-                        <input type="password" className="form-control" name="confirmPassword" id="confirmPassword" 
-                               onChange={ onConfirmPasswordChange } />
-                    </div>
-                    <button type="submit" className="btn btn-primary">
-                        Отправить
-                    </button>
-                    <Link className="btn btn-success" to="/login">Войти</Link>
-                    
-                    <ValidationErrors validationErrors={ validationErrors } />
-
-                </form>
-            </div>
-        );
+        return <RegisterFormComponent {...props}  />;
     }
 }
 
