2b047add763d4e54312b414f758e1eaf5cfc69ac
maksimm <maksim1989sovunion@yandex.ru>
2017-11-29 15:07:40 +0300

    start working on more strict client-side

--- react-redux-ts/client/reducers/user.ts
+++ react-redux-ts/client/reducers/user.ts
@@ -2,9 +2,9 @@ import { LOGIN_SUCCESS } from '../actions/constants';
 import { AuthActions } from '../actions/authorization';
 import UserView from '../../app/models/UserView';
 
-const notAuthorized: UserView = null;
+const notAuthorized = null;
 
-const user: (state: UserView, action: AuthActions[keyof AuthActions]) => UserView = 
+const user: (state: UserView | null, action: AuthActions[keyof AuthActions]) => UserView | null = 
         (state = notAuthorized, action) => {
     switch (action.type) {
         case LOGIN_SUCCESS:
