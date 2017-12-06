b3c2868218c1a8572f7860fc44e00208c7227741
maksimn <maksim1989sovunion@yandex.ru>
2017-12-06 08:07:11 +0300

    Security

--- react-redux-ts/client/components/App.tsx
+++ react-redux-ts/client/components/App.tsx
@@ -3,9 +3,11 @@ import { Route } from 'react-router-dom';
 
 import Header from './Header';
 import MainContent from './MainContent';
+import Security from '../containers/Security';
 
 const App = () => (
     <div>
+        <Route path="*" component={ Security } />
         <Route path="*" component={ Header } />
         <MainContent />
     </div>
---
+++ react-redux-ts/client/containers/Security.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import { AppState } from '../store/AppState';
+
+class Security extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        const { user } = this.props,
+           url = this.props.location.pathname,
+           isAuthenticated = user !== null;
+
+        if (isAuthenticated && (url === '/login' || url === '/register')) {
+            return <Redirect to="/photos" />;
+        } else if (!isAuthenticated && (url === '/photos' || url === '/profile')) {
+            return <Redirect to="/login" />;
+        }
+
+        return null;
+    }
+}
+
+export default connect(
+    (state: AppState) => ({ user: state.user })
+)(Security);
