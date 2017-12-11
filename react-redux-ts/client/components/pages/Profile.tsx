51cedd1170601aab560b57eda13de9324170febe
maksimm <maksim1989sovunion@yandex.ru>
2017-12-11 19:11:56 +0300

    Profile page

--- react-redux-ts/client/components/pages/Profile.tsx
+++ react-redux-ts/client/components/pages/Profile.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
+import { ProfileProps } from '../../containers/pages/Profile';
 
-const Profile = () =>
-    <div className="col-lg-offset-1">
-        <h3>user.name</h3>
-        <div>ID: user.id</div>
-    </div>;
+const Profile: React.StatelessComponent<ProfileProps> = props => {
+    const { user } = props,
+        username = user ? user.name : '',
+        userId = user ? user.id : '';
+
+    return (<div className="col-lg-offset-1">
+        <h3>{ username } </h3>
+        <div>ID: { userId }</div>
+    </div>);
+};
 
 export default Profile;
--- react-redux-ts/client/containers/pages/Profile.tsx
+++ react-redux-ts/client/containers/pages/Profile.tsx
@@ -1,14 +1,27 @@
 import * as React from 'react';
 import ProfileComponent from '../../components/pages/Profile';
+import UserView from '../../../app/models/UserView';
+import { connect } from 'react-redux';
+import { AppState } from '../../store/AppState';
 
-class Profile extends React.Component<any, any> {
-    constructor(props: any) {
+export interface ProfileProps {
+    user: UserView;
+}
+
+type ProfileState = {};
+
+class Profile extends React.Component<ProfileProps, ProfileState> {
+    constructor(props: ProfileProps) {
         super(props);
     }
 
-    public render() {
-        return <ProfileComponent/>;
+    render() {
+        const { user } = this.props;
+
+        return <ProfileComponent user={ user }/>;
     }
 }
 
-export default Profile;
+export default connect(
+    (state: AppState) => ({ user: state.auth.user })
+)(Profile);
