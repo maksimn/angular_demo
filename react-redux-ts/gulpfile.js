25ffb823a90929c8917bf946348a8e5960671cb2
maksimm <maksim1989sovunion@yandex.ru>
2017-12-13 12:56:01 +0300

    webpack from gulp

--- react-redux-ts/gulpfile.js
+++ react-redux-ts/gulpfile.js
@@ -1,16 +1,29 @@
 var gulp = require('gulp');
 var open = require('gulp-open');
 var nodemon = require('gulp-nodemon');
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+var webpackConfig = require('./webpack.config');
 
-gulp.task('nodestart', function () {
+gulp.task("webpack", function(callback) {
+    webpack(webpackConfig, function(err, stats) {
+        if(err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString({
+            // output options
+        }));
+        callback();
+    });
+});
+
+gulp.task('nodestart', ['webpack'], function () {
     nodemon({
         script: 'app.js'
     });
 });
 
-gulp.task('open', ['nodestart'], function() {
+gulp.task('open', ['webpack', 'nodestart'], function() {
     gulp.src(__filename) 
         .pipe(open({ uri: 'http://localhost:8000/' }));
 });
 
-gulp.task('default', ['nodestart', 'open']);
+gulp.task('default', ['webpack', 'nodestart', 'open']);
