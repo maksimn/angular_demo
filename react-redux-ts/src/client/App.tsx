4d491fa8dfc0c576dafd3847ae368984d85d1d8a
maksimm <maksim1989sovunion@yandex.ru>
2017-12-15 18:27:15 +0300

    move App/tsx file

--- react-redux-ts/src/client/App.tsx
+++ react-redux-ts/src/client/App.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 
-import Header from './common/Header';
-import MainContent from './common/MainContent';
-import Security from '../containers/common/Security';
+import Header from './components/common/Header';
+import MainContent from './components/common/MainContent';
+import Security from './containers/common/Security';
 
 const App = () => (
     <div>
--- react-redux-ts/src/client/index.tsx
+++ react-redux-ts/src/client/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from 'react-redux';
 
-import App from './components/App';
+import App from './App';
 import store from './store';
 
 ReactDOM.render(
